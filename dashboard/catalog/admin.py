611f84c077f8f89231efaf229bd297810533e404
Xander Apponi <xapponi@gmail.com>
2018-11-02 12:00:21 -0500

    Removed old catalog models

--- dashboard/catalog/admin.py
+++ dashboard/catalog/admin.py
@@ -3,41 +3,41 @@ from django.contrib import admin
 # Register your models here.
 from catalog.models import *
 
-admin.site.register(Language)
-#admin.site.register(Book)
-# admin.site.register(Author)
-admin.site.register(Genre)
-#admin.site.register(BookInstance)
+# admin.site.register(Language)
+# #admin.site.register(Book)
+# # admin.site.register(Author)
+# admin.site.register(Genre)
+# #admin.site.register(BookInstance)
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
-
-#Define the admin class
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-# Register the admin class with associated model
-admin.site.register(Author, AuthorAdmin)
-
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-    inlines = [BooksInstanceInline]
-
-# Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
-    fieldsets = (
-        (None, {
-            'fields': ('book', 'imprint', 'id')
-        }),
-        ('Availability', {
-            'fields': ('status', 'due_back')
-        }),
-    )
+# class BooksInstanceInline(admin.TabularInline):
+#     model = BookInstance
+#
+# #Define the admin class
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+#
+# # Register the admin class with associated model
+# admin.site.register(Author, AuthorAdmin)
+#
+# # Register the Admin classes for Book using the decorator
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'display_genre')
+#     inlines = [BooksInstanceInline]
+#
+# # Register the Admin classes for BookInstance using the decorator
+# @admin.register(BookInstance)
+# class BookInstanceAdmin(admin.ModelAdmin):
+#     list_filter = ('status', 'due_back')
+#     fieldsets = (
+#         (None, {
+#             'fields': ('book', 'imprint', 'id')
+#         }),
+#         ('Availability', {
+#             'fields': ('status', 'due_back')
+#         }),
+#     )
 
 @admin.register(GroupVideo)
 class GroupVideoAdmin(admin.ModelAdmin):
